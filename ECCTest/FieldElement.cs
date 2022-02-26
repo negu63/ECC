eb9f1051e036cf15bfc41eee03f61b00c0d3e4e6
negu63 <negu6633@gmail.com>
2022-02-26 16:06:23 +0900

    Create test class of FieldElement

--- ECCTest/FieldElement.cs
+++ ECCTest/FieldElement.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ECCTest
 {
     [TestClass]
-    public class UnitTest1
+    public class FieldElement
     {
         [TestMethod]
         public void TestMethod1()
