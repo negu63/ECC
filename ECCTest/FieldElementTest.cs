2d548140832c3a3e0f3dbd97a39f5ba22ddf0f03
negu63 <negu6633@gmail.com>
2022-02-26 17:13:04 +0900

    [FieldElement] Remove test class magic number - prime

--- ECCTest/FieldElementTest.cs
+++ ECCTest/FieldElementTest.cs
@@ -7,10 +7,12 @@ namespace ECCTest
     [TestClass]
     public class FieldElementTest
     {
+        readonly BigInteger prime = 31;
+
         [TestMethod]
         public void ToString_ReturnsFormattedString()
         {
-            FieldElement f1 = new(2, 31);
+            FieldElement f1 = new(2, prime);
 
             Assert.IsTrue(f1.ToString() == "FieldElement_31(2)");
         }
@@ -20,8 +22,8 @@ namespace ECCTest
         [DataRow("2", "15")]
         public void GetHashCode_ReturnsCorrectHashCode(string num1, string num2)
         {
-            FieldElement f1 = new(BigInteger.Parse(num1), 31);
-            FieldElement f2 = new(BigInteger.Parse(num2), 31);
+            FieldElement f1 = new(BigInteger.Parse(num1), prime);
+            FieldElement f2 = new(BigInteger.Parse(num2), prime);
 
             bool isSameHashCode = f1.GetHashCode() == f2.GetHashCode();
 
@@ -40,8 +42,8 @@ namespace ECCTest
         [DataRow("2", "15")]
         public void Equals_ReturnsTrueOnlyIfTheyAreEqual(string num1, string num2)
         {
-            FieldElement f1 = new(BigInteger.Parse(num1), 31);
-            FieldElement f2 = new(BigInteger.Parse(num2), 31);
+            FieldElement f1 = new(BigInteger.Parse(num1), prime);
+            FieldElement f2 = new(BigInteger.Parse(num2), prime);
 
             bool areEqual = f1.Equals(f2);
 
