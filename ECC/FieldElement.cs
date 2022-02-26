f38ebcd3276f6a64778fa901b0981915890f8f40
negu63 <negu6633@gmail.com>
2022-02-26 16:17:16 +0900

    Add FieldElement constructor

--- ECC/FieldElement.cs
+++ ECC/FieldElement.cs
@@ -7,5 +7,22 @@ namespace ECC
     {
         private BigInteger Num { get; set; }
         private BigInteger Prime { get; set; }
+
+        public FieldElement(BigInteger num, BigInteger prime)
+        {
+            try
+            {
+                if (num >= prime || num < 0)
+                {
+                    throw new Exception($"Num {num} not in field range 0 to {prime - 1}");
+                }
+                this.Num = num;
+                this.Prime = prime;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
     }
 }
