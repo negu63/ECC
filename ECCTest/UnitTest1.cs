8c8c6e1429d1fc8b89e8f8347bf996394773c21c
negu63 <negu6633@gmail.com>
2022-02-26 16:04:23 +0900

    Create ECC MSTest Project

---
+++ ECCTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ECCTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
