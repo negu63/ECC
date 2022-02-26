8a85638cbf474ebcb87c36185a6e538b0954e54f
negu63 <negu6633@gmail.com>
2022-02-26 16:55:50 +0900

    [FieldElement] Add GetHashCode method test

--- ECCTest/FieldElementTest.cs
+++ ECCTest/FieldElementTest.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ECC;
+using System.Numerics;
 
 namespace ECCTest
 {
@@ -12,6 +13,27 @@ namespace ECCTest
             FieldElement f1 = new(2, 31);
 
             Assert.IsTrue(f1.ToString() == "FieldElement_31(2)");
+        }
+
+        [TestMethod]
+        [DataRow("2", "2")]
+        [DataRow("2", "15")] 
+        public void GetHashCode_ReturnsCorrectHashCode(string num1, string num2)
+        {
+
+            FieldElement f1 = new(BigInteger.Parse(num1), 31);
+            FieldElement f2 = new(BigInteger.Parse(num2), 31);
+
+            bool isSameHashCode = f1.GetHashCode() == f2.GetHashCode();
+
+            if (num1 == num2)
+            {
+                Assert.IsTrue(isSameHashCode);
+            }
+            else
+            {
+                Assert.IsFalse(isSameHashCode);
+            }
         }
     }
 }
