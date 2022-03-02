18deeb62af465ee373e1eb6e95cf27845ca55688
negu63 <negu6633@gmail.com>
2022-03-03 00:59:14 +0900

    [Point] Add constructor

--- ECC/Point.cs
+++ ECC/Point.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ECC
 {
-    class Point
+    public class Point
     {
+        public BigInteger X { get; private set; }
+        public BigInteger Y { get; private set; }
+        public BigInteger A { get; private set; }
+        public BigInteger B { get; private set; }
+
+        public Point(BigInteger x, BigInteger y, BigInteger a, BigInteger b)
+        {
+            try
+            {
+                X = x;
+                Y = y;
+                A = a;
+                B = b;
+                if (BigInteger.Pow(Y, 2) != (BigInteger.Pow(X, 3) + a * x + b))
+                {
+                    throw new Exception($"({X}, {Y}) is not on the curve");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+            }
+
+        }
     }
 }
