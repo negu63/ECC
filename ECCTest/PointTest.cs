1a1188d13b220663761232fcfafdc74c14c8798d
negu63 <negu6633@gmail.com>
2022-03-15 01:41:51 +0900

    [Point] Add Equals method test

--- ECCTest/PointTest.cs
+++ ECCTest/PointTest.cs
@@ -24,10 +24,10 @@ namespace ECCTest
                     new object[] { true, new Point[] {
                         new Point(new FieldElement(192, 223), new FieldElement(105, 223), new FieldElement(0, 223), new FieldElement(7, 223)),
                         new Point(new FieldElement(192, 223), new FieldElement(105, 223), new FieldElement(0, 223), new FieldElement(7, 223)) } },
-                     new object[] { false, new Point[] {
+                    new object[] { false, new Point[] {
                         new Point(new FieldElement(1, 223), new FieldElement(193, 223), new FieldElement(0, 223), new FieldElement(7, 223)),
                         new Point(new FieldElement(17, 223), new FieldElement(56, 223), new FieldElement(0, 223), new FieldElement(7, 223)) } },
-                     new object[] { true, new Point[] { new Point(null, null, 5, 7), new Point(null, null, 5, 7) } }
+                    new object[] { true, new Point[] { new Point(null, null, 5, 7), new Point(null, null, 5, 7) } }
                 };
             }
         }
@@ -45,10 +45,38 @@ namespace ECCTest
             {
                 return new[] {
                     new object[] { "Point(infinity)", new Point(null, null, 5, 7) },
-                    new object[] { 
-                        "Point(192, 105)_0_7 FieldElement(223)", 
+                    new object[] {
+                        "Point(192, 105)_0_7 FieldElement(223)",
                         new Point(new FieldElement(192, 223), new FieldElement(105, 223), new FieldElement(0, 223), new FieldElement(7, 223)) },
                     new object[] { "Point(3, -7)_5_7", new Point(3, -7, 5, 7) }
+                };
+            }
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(EqualsInput))]
+        public void Equals_ReturnsTrueOnlyIfTheyAreEqual(bool expected, Point[] points)
+        {
+            Assert.AreEqual(expected, points[0].Equals(points[1]));
+        }
+
+
+        [TestMethod]
+        [DynamicData(nameof(EqualsInput))]
+        public void OverrideEquals_ReturnsTrueOnlyIfTheyAreEqual(bool expected, object[] points)
+        {
+            bool result = ((Point)points[0]).Equals(points[1]);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        public static IEnumerable<object[]> EqualsInput
+        {
+            get
+            {
+                return new[] {
+                    new object[] { true, new Point[] { new Point(3, -7, 5, 7), new Point(3, -7, 5, 7) } },
+                    new object[] { false, new Point[] { new Point(3, -7, 5, 7), new Point(18, 77, 5, 7) } },
                 };
             }
         }
