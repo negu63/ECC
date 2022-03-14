cff006e345b81356e0870b1e5cd2ff239167e606
negu63 <negu6633@gmail.com>
2022-03-15 02:46:27 +0900

    [Point] Add Add operator

--- ECC/Point.cs
+++ ECC/Point.cs
@@ -128,5 +128,74 @@ namespace ECC
         {
             return !Equals(p1, p2);
         }
+
+        public static Point operator +(Point p1, Point p2)
+        {
+            try
+            {
+                if (p1.A != p2.A || p1.B != p2.B)
+                {
+                    throw new Exception($"Points {p1}, {p2} are not on the same curve");
+                }
+                else if (p1.X == null)
+                {
+                    return p2;
+                }
+                else if (p2.X == null)
+                {
+                    return p1;
+                }
+                else if (p1.X == p2.X && p1.Y != p2.Y)
+                {
+                    return new Point((object)null, null, p1.A, p1.B);
+                }
+                else if (p1.X != p2.X)
+                {
+                    dynamic s = (p1.Y - p2.Y) / (p1.X - p2.X);
+                    if (p1.X.GetType() == typeof(FieldElement))
+                    {
+                        FieldElement x = ((FieldElement)s).Pow(2) - p1.X - p2.X;
+                        FieldElement y = s * (p1.X - x) - p1.Y;
+                        return new Point(x, y, p1.A, p1.B);
+                    }
+                    else
+                    {
+                        BigInteger x = BigInteger.Pow(s, 2) - p1.X - p2.X;
+                        BigInteger y = s * (p1.X - x) - p1.Y;
+                        return new Point(x, y, p1.A, p1.B);
+                    }
+                }
+                else if (p1 == p2 && p1.Y == 0 * p1.X)
+                {
+                    return new Point((object)null, null, p1.X, p1.Y);
+                }
+                else if (p1 == p2)
+                {
+                    if (p1.X.GetType() == typeof(FieldElement))
+                    {
+                        FieldElement s = (3 * p1.X.Pow(2) + p1.A) / (2 * p1.Y);
+                        FieldElement x = s.Pow(2) - 2 * p1.X;
+                        FieldElement y = s * (p1.X - x) - p1.Y;
+                        return new Point(x, y, p1.A, p1.B);
+                    }
+                    else
+                    {
+                        BigInteger s = (3 * BigInteger.Pow(p1.X, 2) + p1.A) / (2 * p1.Y);
+                        BigInteger x = BigInteger.Pow(s, 2) - 2 * p1.X;
+                        BigInteger y = s * (p1.X - x) - p1.Y;
+                        return new Point(x, y, p1.A, p1.B);
+                    }
+                }
+                else
+                {
+                    throw new Exception($"Points {p1}, {p2} can't be added");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
     }
 }
