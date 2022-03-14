75e51d1fa8735d7d71c0da10ba41519a6e0d641c
negu63 <negu6633@gmail.com>
2022-03-14 19:41:03 +0900

    [Point] Add GetHashCode test case for Infinity Point

--- ECCTest/PointTest.cs
+++ ECCTest/PointTest.cs
@@ -21,12 +21,13 @@ namespace ECCTest
                 return new[] {
                     new object[] { true,  new Point[] { new Point(3, -7, 5, 7), new Point(3, -7, 5, 7) } },
                     new object[] { false,  new Point[] { new Point(3, -7, 5, 7), new Point(18, 77, 5, 7) } },
-                    new object[] { true, new Point[] { 
-                        new Point(new FieldElement(192, 223), new FieldElement(105, 223), new FieldElement(0, 223), new FieldElement(7, 223)), 
+                    new object[] { true, new Point[] {
+                        new Point(new FieldElement(192, 223), new FieldElement(105, 223), new FieldElement(0, 223), new FieldElement(7, 223)),
                         new Point(new FieldElement(192, 223), new FieldElement(105, 223), new FieldElement(0, 223), new FieldElement(7, 223)) } },
                      new object[] { false, new Point[] {
                         new Point(new FieldElement(1, 223), new FieldElement(193, 223), new FieldElement(0, 223), new FieldElement(7, 223)),
-                        new Point(new FieldElement(17, 223), new FieldElement(56, 223), new FieldElement(0, 223), new FieldElement(7, 223)) } }
+                        new Point(new FieldElement(17, 223), new FieldElement(56, 223), new FieldElement(0, 223), new FieldElement(7, 223)) } },
+                     new object[] { true, new Point[] { new Point(null, null, 5, 7), new Point(null, null, 5, 7) } }
                 };
             }
         }
