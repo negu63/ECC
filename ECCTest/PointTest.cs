a3e5207459d23fdd95ae94910e11bbf735fec980
negu63 <negu6633@gmail.com>
2022-03-15 03:01:18 +0900

    [Point] Refactor: Improve test input readability

--- ECCTest/PointTest.cs
+++ ECCTest/PointTest.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ECC;
 using System.Collections.Generic;
+using System.Numerics;
 
 namespace ECCTest
 {
@@ -20,16 +21,26 @@ namespace ECCTest
         {
             get
             {
+                BigInteger prime = 223;
+                BigInteger a = 5;
+                BigInteger b = 7;
+                Point p1 = new(3, -7, a, b);
+                Point p2 = new(18, 77, a, b);
+                FieldElement f1 = new(192, prime);
+                FieldElement f2 = new(105, prime);
+                FieldElement f3 = new(0, prime);
+                FieldElement f4 = new(7, prime);
+                FieldElement f5 = new(1, prime);
+                FieldElement f6 = new(193, prime);
+                FieldElement f7 = new(17, prime);
+                FieldElement f8 = new(56, prime);
+
                 return new[] {
-                    new object[] { true,  new Point[] { new Point(3, -7, 5, 7), new Point(3, -7, 5, 7) } },
-                    new object[] { false,  new Point[] { new Point(3, -7, 5, 7), new Point(18, 77, 5, 7) } },
-                    new object[] { true, new Point[] {
-                        new Point(new FieldElement(192, 223), new FieldElement(105, 223), new FieldElement(0, 223), new FieldElement(7, 223)),
-                        new Point(new FieldElement(192, 223), new FieldElement(105, 223), new FieldElement(0, 223), new FieldElement(7, 223)) } },
-                    new object[] { false, new Point[] {
-                        new Point(new FieldElement(1, 223), new FieldElement(193, 223), new FieldElement(0, 223), new FieldElement(7, 223)),
-                        new Point(new FieldElement(17, 223), new FieldElement(56, 223), new FieldElement(0, 223), new FieldElement(7, 223)) } },
-                    new object[] { true, new Point[] { new Point(null, null, 5, 7), new Point(null, null, 5, 7) } }
+                    new object[] { true,  new Point[] { p1, p1 } },
+                    new object[] { false,  new Point[] { p1, p2 } },
+                    new object[] { true, new Point[] { new Point(f1, f2, f3, f4), new Point(f1, f2, f3, f4) } },
+                    new object[] { false, new Point[] { new Point(f5, f6, f3, f4), new Point(f7, f8, f3, f4) } },
+                    new object[] { true, new Point[] { new Point(null, null, a, b), new Point(null, null, a, b) } }
                 };
             }
         }
@@ -47,12 +58,18 @@ namespace ECCTest
         {
             get
             {
+                BigInteger prime = 223;
+                BigInteger a = 5;
+                BigInteger b = 7;
+                FieldElement f1 = new(192, prime);
+                FieldElement f2 = new(105, prime);
+                FieldElement f3 = new(0, prime);
+                FieldElement f4 = new(7, prime);
+
                 return new[] {
-                    new object[] { "Point(infinity)", new Point(null, null, 5, 7) },
-                    new object[] {
-                        "Point(192, 105)_0_7 FieldElement(223)",
-                        new Point(new FieldElement(192, 223), new FieldElement(105, 223), new FieldElement(0, 223), new FieldElement(7, 223)) },
-                    new object[] { "Point(3, -7)_5_7", new Point(3, -7, 5, 7) }
+                    new object[] { "Point(infinity)", new Point(null, null, a, b) },
+                    new object[] { $"Point(192, 105)_0_7 FieldElement({prime})", new Point(f1, f2, f3, f4) },
+                    new object[] { $"Point(3, -7)_{a}_{b}", new Point(3, -7, a, b) }
                 };
             }
         }
@@ -97,9 +114,14 @@ namespace ECCTest
         {
             get
             {
+                BigInteger a = 5;
+                BigInteger b = 7;
+                Point p1 = new(3, -7, a, b);
+                Point p2 = new(18, 77, a, b);
+
                 return new[] {
-                    new object[] { true, new Point[] { new Point(3, -7, 5, 7), new Point(3, -7, 5, 7) } },
-                    new object[] { false, new Point[] { new Point(3, -7, 5, 7), new Point(18, 77, 5, 7) } },
+                    new object[] { true, new Point[] { p1, p1 } },
+                    new object[] { false, new Point[] { p1, p2 } },
                 };
             }
         }
@@ -117,9 +139,11 @@ namespace ECCTest
         {
             get
             {
-                Point p1 = new((object)null, null, 5, 7);
-                Point p2 = new(2, 5, 5, 7);
-                Point p3 = new(2, -5, 5, 7);
+                BigInteger a = 5;
+                BigInteger b = 7;
+                Point p1 = new((object)null, null, a, b);
+                Point p2 = new(2, 5, a, b);
+                Point p3 = new(2, -5, a, b);
 
                 return new[]
                 {
